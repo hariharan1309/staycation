2c8cffec076c7dfa40df9ac353a7101e9710e27a
hariharan1309 <63937651+hariharan1309@users.noreply.github.com>
2025-03-28 14:53:19 +0530

    Loader Improvements

--- src/app/loading.tsx
+++ src/app/loading.tsx
@@ -1,4 +1,84 @@
-export default function Loading() {
-  return null
-}
+export default function Loader() {
+  return (
+    <section className="p-10">
+      <div className="mt-8 grid grid-cols-1 gap-8 sm:grid-cols-2 lg:grid-cols-3 xl:mt-12 xl:grid-cols-4 xl:gap-12">
+        <div className="w-full">
+          <div className="h-40 w-full animate-pulse rounded-lg bg-gray-300 dark:bg-gray-600"></div>
+        </div>
 
+        <div className="w-full">
+          <div className="h-40 w-full animate-pulse rounded-lg bg-gray-300 dark:bg-gray-600"></div>
+        </div>
+
+        <div className="w-full">
+          <div className="h-40 w-full animate-pulse rounded-lg bg-gray-300 dark:bg-gray-600"></div>
+        </div>
+
+        <div className="w-full">
+          <div className="h-40 w-full animate-pulse rounded-lg bg-gray-300 dark:bg-gray-600"></div>
+        </div>
+      </div>
+      <div className="mt-8 grid grid-cols-1 gap-8 lg:grid-cols-2 xl:mt-12 xl:gap-12">
+        <div
+          role="status"
+          className="animate-pulse rounded border border-gray-300 p-4 shadow md:p-6 dark:border-gray-600"
+        >
+          <div className="mb-2.5 h-2.5 w-32 rounded-full bg-gray-300 dark:bg-gray-600"></div>
+          <div className="mb-10 h-2 w-48 rounded-full bg-gray-300 dark:bg-gray-600"></div>
+          <div className="mt-4 flex items-baseline">
+            <div className="h-72 w-full rounded-t-lg bg-gray-300 dark:bg-gray-600"></div>
+            <div className="ms-6 h-56 w-full rounded-t-lg bg-gray-300 dark:bg-gray-600"></div>
+            <div className="ms-6 h-72 w-full rounded-t-lg bg-gray-300 dark:bg-gray-600"></div>
+            <div className="ms-6 h-64 w-full rounded-t-lg bg-gray-300 dark:bg-gray-600"></div>
+            <div className="ms-6 h-80 w-full rounded-t-lg bg-gray-300 dark:bg-gray-600"></div>
+            <div className="ms-6 h-72 w-full rounded-t-lg bg-gray-300 dark:bg-gray-600"></div>
+            <div className="ms-6 h-80 w-full rounded-t-lg bg-gray-300 dark:bg-gray-600"></div>
+          </div>
+          <span className="sr-only">Loading...</span>
+        </div>
+
+        <div
+          role="status"
+          className="animate-pulse space-y-4 divide-y divide-gray-300 rounded border border-gray-300 p-4 shadow md:p-6 dark:divide-gray-600 dark:border-gray-600"
+        >
+          <div className="flex items-center justify-between">
+            <div>
+              <div className="mb-2.5 h-2.5 w-24 rounded-full bg-gray-300 dark:bg-gray-600"></div>
+              <div className="h-2 w-32 rounded-full bg-gray-300 dark:bg-gray-600"></div>
+            </div>
+            <div className="h-2.5 w-12 rounded-full bg-gray-300 dark:bg-gray-600"></div>
+          </div>
+          <div className="flex items-center justify-between pt-4">
+            <div>
+              <div className="mb-2.5 h-2.5 w-24 rounded-full bg-gray-300 dark:bg-gray-600"></div>
+              <div className="h-2 w-32 rounded-full bg-gray-300 dark:bg-gray-600"></div>
+            </div>
+            <div className="h-2.5 w-12 rounded-full bg-gray-300 dark:bg-gray-600"></div>
+          </div>
+          <div className="flex items-center justify-between pt-4">
+            <div>
+              <div className="mb-2.5 h-2.5 w-24 rounded-full bg-gray-300 dark:bg-gray-600"></div>
+              <div className="h-2 w-32 rounded-full bg-gray-300 dark:bg-gray-600"></div>
+            </div>
+            <div className="h-2.5 w-12 rounded-full bg-gray-300 dark:bg-gray-600"></div>
+          </div>
+          <div className="flex items-center justify-between pt-4">
+            <div>
+              <div className="mb-2.5 h-2.5 w-24 rounded-full bg-gray-300 dark:bg-gray-600"></div>
+              <div className="h-2 w-32 rounded-full bg-gray-300 dark:bg-gray-600"></div>
+            </div>
+            <div className="h-2.5 w-12 rounded-full bg-gray-300 dark:bg-gray-600"></div>
+          </div>
+          <div className="flex items-center justify-between pt-4">
+            <div>
+              <div className="mb-2.5 h-2.5 w-24 rounded-full bg-gray-300 dark:bg-gray-600"></div>
+              <div className="h-2 w-32 rounded-full bg-gray-300 dark:bg-gray-600"></div>
+            </div>
+            <div className="h-2.5 w-12 rounded-full bg-gray-300 dark:bg-gray-600"></div>
+          </div>
+          <span className="sr-only">Loading...</span>
+        </div>
+      </div>
+    </section>
+  );
+}
