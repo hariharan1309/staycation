265a241baac070a1c9ebf3a06d77af74c0cad86d
hariharan1309 <63937651+hariharan1309@users.noreply.github.com>
2025-03-30 21:59:05 +0530

    Specific Property Route

---
+++ src/app/api/properties/[id]/route.ts
@@ -0,0 +1,33 @@
+import { fstore } from "@/lib/firebase";
+import { collection, doc, getDoc } from "firebase/firestore";
+import { NextResponse } from "next/server";
+
+export const GET = async (req: Request, context: { params: any }) => {
+  try {
+    const id = context.params.id;
+    const data = await getDoc(doc(fstore, "property", id));
+    if (!data.exists()) {
+      return new NextResponse(
+        JSON.stringify({
+          message: "Not Found...",
+          status: 404,
+        })
+      );
+    }
+    const propertyData = { id: id, ...data.data() };
+    return new NextResponse(
+      JSON.stringify({
+        property: propertyData,
+        message: "Retrieved Successfully",
+        status: 200,
+      })
+    );
+  } catch (error) {
+    new NextResponse(
+      JSON.stringify({
+        message: "Retrieved Successfully",
+        status: 500,
+      })
+    );
+  }
+};
